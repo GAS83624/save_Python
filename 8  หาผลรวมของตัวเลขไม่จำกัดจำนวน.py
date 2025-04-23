d2bc1be3811b0e0132174e13433cb9fa909df005
SOMCHENG <124430027+GAS83624@users.noreply.github.com>
2025-04-23 12:05:09 +0700

    ป้อนตัวเลข

--- 8  หาผลรวมของตัวเลขไม่จำกัดจำนวน.py
+++ 8  หาผลรวมของตัวเลขไม่จำกัดจำนวน.py
@@ -1,7 +1,7 @@
 #หาผลรวมของตัวเลขไม่จำกัดจำนวน
 ans = 0
 while True:
-    number = int(input("ป้อนตัวเลข :"))
+    number = int(input("ป้อนตัวเลขของคุณ :"))
     if number<=0:
         break
     ans += number
