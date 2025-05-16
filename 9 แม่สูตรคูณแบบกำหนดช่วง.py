967413753e513b5052b0f717530be1ad5011f75f
SOMCHENG <pongji8749@gmail.com>
2025-05-16 20:23:29 +0700

    Update 9 แม่สูตรคูณแบบกำหนดช่วง.py

--- 9 แม่สูตรคูณแบบกำหนดช่วง.py
+++ 9 แม่สูตรคูณแบบกำหนดช่วง.py
@@ -2,8 +2,8 @@
 start=int(input("ตั้งแต่:"))
 end=int(input("จนถึง:"))
 for i in range(start,  end+1):
-    print("=================")
+    print("================")
     print("สูตรคูณแม่:", i)
-    print("=================")
+    print("================")
     for o in range(1, 13):
         print(i, "x", o,"=", i*o)
